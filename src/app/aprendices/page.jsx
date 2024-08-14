a18116bffea3888b96257f17f99c61dd89148daa
ortizpablos <ortizpablos@gmail.com>
2024-08-14 07:03:54 -0500

    subida del get

--- src/app/aprendices/page.jsx
+++ src/app/aprendices/page.jsx
@@ -1,10 +1,11 @@
+import GetAprendicesComponent from "@/components/GetAprendicesComponent";
 
 
 
 export default function page() {
   return (
     <>
-       asdad
+       <GetAprendicesComponent />
     </>
   )
 }
