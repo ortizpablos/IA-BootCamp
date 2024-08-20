25aca84ab409f261e14c2300e33d7f580e7800f7
ortizpablos <ortizpablos@gmail.com>
2024-08-20 08:18:56 -0500

    favicon

--- src/components/BannerCompoenent.jsx
+++ src/components/BannerCompoenent.jsx
@@ -12,8 +12,8 @@ export default function BannerCompoenent() {
                     <br></br>
                     <br></br>
                     <br></br>
-                <h1 className="text-5xl font-bold text-white pt-3">Bootcamp y Hackathon</h1>
-                <h2 className="text-4xl font-bold text-white pt-0.5">en Inteligencia Artificial</h2>
+                <h1 className="text-4xl font-bold text-white pt-3">Bootcamp y Hackathon</h1>
+                <h2 className="text-2xl font-bold text-white pt-0.5">en Inteligencia Artificial</h2>
                 <p className=" text-1xl py-4 text-white">
                    <i> Serán días cargados de conocimiento que te permitirán Adquirir habilidades técnicas y socioemocionales "Todo un Reto" </i> 
                     </p>   
