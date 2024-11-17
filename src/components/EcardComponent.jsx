ceda964d5feca09cef16dcd840c1a734619c0266
ortizpablos <ortizpablos@gmail.com>
2024-11-17 11:41:24 -0500

    cambio de ecard

--- src/components/EcardComponent.jsx
+++ src/components/EcardComponent.jsx
@@ -7,7 +7,7 @@ export default function EcardComponent() {
             <div className="hero-content flex-col w-full lg:flex-row-reverse p-1">
                 <div className="card  w-full max-w-sm shrink-0 shadow-2xl ">
 
-                <img src="./esca.jpg" alt="ecard" />
+                <img src="./pendon.jpg" alt="Ecard" />
                 </div>
             </div>
         </div>
