c48781b2d099b26872c9cbf22bf0b177dcbc49f0
ortizpablos <ortizpablos@gmail.com>
2024-11-19 14:13:38 -0500

    cambio footer quitar qr

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -4,20 +4,9 @@ export default function FooterComponent() {
   return (
     <>
         <footer className="footer footer-center text-base-content rounded p-10">
-        <h1 className='text-4xl font-bold text-center text-white className="m-0"' id='estrategia'>ÚNETE AL GRUPO</h1>
+        
             
-          <section className="m-0 p-0">
-          <Image
-              src="/qr.png"
-              alt="qr"
-              className="dark:invert m-0 p-0"
-              width={400}
-              height={24}
-              priority
-            />
-          </section>
-          
-          <Link className="text-white" href="https://chat.whatsapp.com/IAPeyPpZCch5R6dmTiS9FL">o haz clic Aquí para Unirte al Grupo</Link>
+                
           <hr />
           <aside className="text-white">
             <p>Copyright © {new Date().getFullYear()} - Todos los derechos reservados SENA</p>
